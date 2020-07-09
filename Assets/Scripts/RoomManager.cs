8da7d5f2e6c92f1cbec800811c84bfacfe90d461
LiamMcKinney <ljmckinney02@gmail.com>
2020-07-09 12:59:53 -0500

    made actual dungeons

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class RoomManager : MonoBehaviour {
     public int dungeonSize;
-    public Vector2 roomSize;
+    public Vector2 roomSize;//dimensions of a room
     Dictionary<Vector2, Room> layout;
 
     public GameObject roomPrefab;
     public GameObject hallwayPrefab;
     public float roomSpacingScale;
     public float hallwayOffset;
+
+    public Tilemap grid;
+    public Tile wallTile;
 
 	// Use this for initialization
 	void Start () {
@@ -54,25 +58,39 @@ public class RoomManager : MonoBehaviour {
     {
         foreach(Vector2 key in layout.Keys)
         {
-            Instantiate(roomPrefab, key * roomSpacingScale, Quaternion.identity);
+            //Instantiate(roomPrefab, key * roomSpacingScale, Quaternion.identity);
 
-            List<Vector2> usedExits = new List<Vector2>(new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right });
-            foreach(Vector2 exit in layout[key].openExits)
+            //List<Vector2> usedExits = new List<Vector2>(new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right });
+            /*foreach(Vector2 exit in layout[key].openExits)
             {
                 usedExits.Remove(exit);
-            }
+            }*/
 
-            foreach(Vector2 exit in usedExits)
+            /*foreach(Vector2 exit in usedExits)
             {
                 Instantiate(hallwayPrefab, (key * roomSpacingScale) + (exit * hallwayOffset), Quaternion.identity);
-            }
+            }*/
+            CreateRoom(key, layout[key].openExits);
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void CreateRoom(Vector2 position, List<Vector2> walledExits)
+    {
+        Vector2 baseCorner = new Vector2(position.x * roomSize.x, position.y * roomSize.y);//coordinates of the bottom left corner of the room.
+        Vector2 exitCoords = new Vector2(baseCorner.x + (int)(roomSize.x / 2), baseCorner.y + (int)(roomSize.y / 2));
+
+        for(int i = (int)baseCorner.x; i < baseCorner.x + roomSize.x; i++)
+        {
+            grid.SetTile(new Vector3Int(i, (int)baseCorner.y, 0), (i==exitCoords.x && !walledExits.Contains(Vector2.down)) ? null : wallTile);
+            grid.SetTile(new Vector3Int(i, (int)(baseCorner.y + roomSize.y) - 1, 0), (i == exitCoords.x && !walledExits.Contains(Vector2.up)) ? null : wallTile);
+        }
+
+        for (int i = (int)baseCorner.y; i < baseCorner.y + roomSize.y; i++)
+        {
+            grid.SetTile(new Vector3Int((int)baseCorner.x, i, 0), (i == exitCoords.y && !walledExits.Contains(Vector2.left)) ? null : wallTile);
+            grid.SetTile(new Vector3Int((int)(baseCorner.x + roomSize.x) - 1, i, 0), (i == exitCoords.y && !walledExits.Contains(Vector2.right)) ? null : wallTile);
+        }
+    }
 
     class Room
     {

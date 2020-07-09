b43a1346dcf9ccd3bae93b65e9f4ea2ad61311cf
Nithead <nithead@yahoo.com>
2020-07-09 12:33:09 -0500

    enemy follows player

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move()
+    {
+        Vector3 oldPos = transform.position;
+        float horiz;
+        float vertic;
+        if(oldPos.x > target.position.x)
+        {
+            horiz = -1;
+        }
+        else if (oldPos.x == target.position.x)
+        {
+            horiz = 0;
+        }
+        else
+        {
+            horiz = 1;
+        }
+        if(oldPos.y > target.position.y)
+        {
+            vertic = -1;
+        }
+        else if(oldPos.y == target.position.y)
+        {
+            vertic = 0;
+        }
+        else
+        {
+            vertic = 1;
+        }
+        transform.position = new Vector3(horiz, vertic, 0)+oldPos;
+    }
+}
--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -6,24 +6,49 @@ public class PlayerBehavior : MonoBehaviour
 {
     float horiz;
     float vertic;
-    int HP;
+    public int hp;
+    int counter;
+    public Camera camera;
+    public Vector3 camOffset;
+    public EnemyManager enemyManager;
+    public bool isDead;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isDead = false;
+        counter = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        counter++;
         horiz = Input.GetAxisRaw("Horizontal");
         vertic = Input.GetAxisRaw("Vertical");
-        Move(horiz, vertic);
+        if (horiz != 0 || vertic != 0)
+        {
+            if (counter > 100)
+            {
+                Move(horiz, vertic);
+                enemyManager.Step();
+                counter = 0;
+            }
+        }
+        camera.transform.position = transform.position + camOffset;
     }
 
     private void Move(float x, float y)
     {
         Vector3 oldPos = transform.position;
         transform.position = new Vector3(oldPos.x+x, oldPos.y+y, oldPos.z);
+    }
+
+    public void Hurt(int dmg)
+    {
+        hp -= dmg;
+        if (hp < 0)
+        {
+            isDead = true;
+        }
     }
 }

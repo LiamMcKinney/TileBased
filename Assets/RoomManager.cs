4a3f743a67e73600cf6942e5a1935170d9bff97b
LiamMcKinney <ljmckinney02@gmail.com>
2020-07-09 11:30:51 -0500

    changed gitignore

--- Assets/RoomManager.cs
+++ Assets/RoomManager.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomManager : MonoBehaviour {
-
+    public int dungeonSize;
+    public Vector2 roomSize;
+    Room[] layout;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        layout = new Room[dungeonSize];
+    }
 	
 	// Update is called once per frame
 	void Update () {
